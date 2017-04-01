dbde211a060f21b43303ca6627c75ee098eda7f1
Xavier Stevens <xstevens@users.noreply.github.com>
2017-04-01 12:58:22 -0700

    Fixing remote-name option

--- src/main.rs
+++ src/main.rs
@@ -18,40 +18,61 @@ fn exit(exit_status: ExitStatus) -> ! {
     process::exit(exit_status as i32);
 }
 
+fn get_filename(url: &str) -> Option<&str> {
+    url.rsplit('/').next()
+}
+
 fn main() {
     let args = App::new("download")
-        .version("0.1.0")
-        .about("remote file downloader command-line interface")
-        .arg(Arg::with_name("output")
-            .short("o")
-            .long("output")
-            .value_name("OUTPUT")
-            .help("output filename")
-            .takes_value(true))
-        .arg(Arg::with_name("remote-name")
-            .short("O")
-            .long("remote-name")
-            .help("output to a file using the same name as the remote"))
-        .arg(Arg::with_name("url").required(true))
-        .get_matches();
+                   .version("0.1.0")
+                   .about("remote file downloader command-line interface")
+                   .arg(Arg::with_name("output")
+                            .short("o")
+                            .long("output")
+                            .value_name("OUTPUT")
+                            .help("output filename")
+                            .takes_value(true))
+                   .arg(Arg::with_name("remote-name")
+                            .short("O")
+                            .long("remote-name")
+                            .help("output to a file using the same name as the remote"))
+                   .arg(Arg::with_name("url").required(true))
+                   .get_matches();
 
-    
     let source_url = args.value_of("url").unwrap();
     let mut res = reqwest::get(source_url)
-        .unwrap_or_else(|_| process::exit(ExitStatus::UrlFailure as i32));
-    if let Some(output_filename) = args.value_of("output") {
-        let _ = File::create(Path::new(output_filename))
+                      .unwrap_or_else(|e| {
+                          let _ = writeln!(&mut std::io::stderr(), "{}", e);
+                          process::exit(ExitStatus::UrlFailure as i32)
+                      });
+
+    let output_filename = {
+        if args.is_present("remote-name") {
+            get_filename(source_url)
+        } else {
+            args.value_of("output")
+        }
+    };
+
+    if let Some(fname) = output_filename {
+        let _ = File::create(Path::new(fname))
             .and_then(|output_file| {
                 let mut writer = BufWriter::new(output_file);
                 io::copy(&mut res, &mut writer)?;
                 writer.flush()?;
                 Ok(())
             })
-            .map_err(|_| exit(ExitStatus::OutputFailure));
+            .map_err(|e| {
+                let _ = writeln!(&mut std::io::stderr(), "{}", e);
+                exit(ExitStatus::OutputFailure);
+            });
     } else {
         io::copy(&mut res, &mut io::stdout())
-            .unwrap_or_else(|_| exit(ExitStatus::OutputFailure));
+            .unwrap_or_else(|e| {
+                let _ = writeln!(&mut std::io::stderr(), "{}", e);
+                exit(ExitStatus::OutputFailure)
+            });
     }
-    
+
     ()
 }
