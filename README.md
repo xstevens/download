9025736154ce4705104ccb48718836c2b62dbf1f
Xavier Stevens <xstevens@users.noreply.github.com>
2017-04-02 13:05:02 -0700

    adding user-agent header option

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ cargo build --release
 
 ## Usage
 ```
-$ target/release/download -h
+$ download -h
 download 0.1.0
 remote file downloader command-line interface
 
@@ -21,15 +21,20 @@ FLAGS:
     -V, --version        Prints version information
 
 OPTIONS:
-    -o, --output <OUTPUT>    output filename
+    -o, --output <OUTPUT>            output filename
+    -U, --user-agent <user-agent>    use value as user-agent header
 
 ARGS:
     <url>
 ```
 
-## Example
+## Examples
 ```
-download -o rustup.sh https://sh.rustup.rs
+$ download -o rustup.sh https://sh.rustup.rs
+```
+
+```
+$ download --user-agent "curl/7.51.0" http://wttr.in
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+#[macro_use]
 extern crate clap;
 extern crate reqwest;
 
 use clap::{Arg, App};
+use reqwest::header::UserAgent;
+use std::fs::File;
 use std::io;
 use std::io::BufWriter;
 use std::io::prelude::*;
-use std::fs::File;
 use std::path::Path;
 use std::process;
+
+static DEFAULT_USER_AGENT: &'static str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
 enum ExitStatus {
     UrlFailure = 1,
@@ -23,29 +27,43 @@ fn get_filename(url: &str) -> Option<&str> {
 }
 
 fn main() {
-    let args = App::new("download")
-                   .version("0.1.0")
-                   .about("remote file downloader command-line interface")
-                   .arg(Arg::with_name("output")
-                            .short("o")
-                            .long("output")
-                            .value_name("OUTPUT")
-                            .help("output filename")
-                            .takes_value(true))
-                   .arg(Arg::with_name("remote-name")
-                            .short("O")
-                            .long("remote-name")
-                            .help("output to a file using the same name as the remote"))
-                   .arg(Arg::with_name("url").required(true))
-                   .get_matches();
-
+    let app = App::new("download")
+                  .version(crate_version!())
+                  .about("remote file downloader command-line interface")
+                  .arg(Arg::with_name("output")
+                           .short("o")
+                           .long("output")
+                           .value_name("OUTPUT")
+                           .help("output filename")
+                           .takes_value(true))
+                  .arg(Arg::with_name("remote-name")
+                           .short("O")
+                           .long("remote-name")
+                           .help("output to a file using the same name as the remote"))
+                  .arg(Arg::with_name("user-agent")
+                           .short("U")
+                           .long("user-agent")
+                           .takes_value(true)
+                           .help("use value as user-agent header"))
+                  .arg(Arg::with_name("url").required(true));
+    let args = app.get_matches();
     let source_url = args.value_of("url").unwrap();
-    let mut res = reqwest::get(source_url)
-                      .unwrap_or_else(|e| {
+    let user_agent = args.value_of("user-agent")
+                         .unwrap_or(DEFAULT_USER_AGENT); 
+
+    let mut client = reqwest::Client::new().unwrap();
+    client.gzip(true);
+    // TODO: add redirect following options
+    client.redirect(reqwest::RedirectPolicy::limited(1));
+    let mut res = client.get(source_url)
+                        .header(UserAgent(user_agent.to_owned()))
+                        .send()
+                        .unwrap_or_else(|e| {
                           let _ = writeln!(&mut std::io::stderr(), "{}", e);
                           process::exit(ExitStatus::UrlFailure as i32)
-                      });
+                        });
 
+    // determine an output filename; if none are set then send to stdout                    
     let output_filename = {
         if args.is_present("remote-name") {
             get_filename(source_url)
